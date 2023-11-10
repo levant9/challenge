4a291a51656492e3dc7092473080653f6bfc363d
Paweł Lewandowski <levant9@gmail.com>
2023-11-10 18:31:24 +0100

    Initialize SpringBoot project with docker DB

---
+++ README.md
@@ -0,0 +1,19 @@
+# Weather forecast provided app
+
+
+## Forecast obtaining
+http://www.7timer.info/doc.php?lang=en
+
+Data must be fetched when the project is initialized and every 1 minute after.
+
+## Output specification
+7 days forecast.
+
+For each day it returns the week day, min and max temperature, wind speed, and wind direction
+
+Stavanger, Norway
+Latitude and longitude coordinates are: 58.969975, 5.733107.
+
+
+## How to run
+Spin PostreSQL database from docker-compose.yml file before starting SpringBoot application.
---
+++ src/test/java/pl/levant/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,23 @@
+package pl.levant.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:tc:postgresql:///dbname"
+})
+class ChallengeApplicationTests {
+
+	@Container
+	private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest");
+	@Test
+	void contextLoads() {
+	}
+
+}
